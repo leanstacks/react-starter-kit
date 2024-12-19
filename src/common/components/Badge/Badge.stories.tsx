6f6faaee862e9bd4288347f1ecf542364b788356
Matthew Warman <warmanmr@gmail.com>
2024-12-19 08:38:00 -0500

    stories

---
+++ src/common/components/Badge/Badge.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Badge from './Badge';
+const meta = {
+  title: 'Common/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: { description: 'The content.' },
+    className: { description: 'Additional CSS classes.' },
+    testId: { description: 'The test identifier.' },
+  },
+  args: {
+    children: '3',
+  },
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Counter: Story = {
+  args: {
+    children: '99+',
+  },
+};
+
+export const Label: Story = {
+  args: {
+    children: 'NEW',
+    className: '!bg-blue-700',
+  },
+};
