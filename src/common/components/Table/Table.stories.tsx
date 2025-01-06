94a4dabc95658ab191888c6c0125fa9249b4a34c
Matthew Warman <warmanmr@gmail.com>
2025-01-06 08:00:51 -0500

    table stories

---
+++ src/common/components/Table/Table.stories.tsx
@@ -0,0 +1,95 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Table from './Table';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+
+const meta = {
+  title: 'Common/Table',
+  component: Table,
+  decorators: [
+    (Story) => (
+      <div className="w-[640px]">
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    columns: { description: 'An array of `ColumnDef`, column definition, objects.' },
+    data: {
+      description:
+        'An array of data objects, of type `TData`, which are used to populate the rows of the table.',
+    },
+    testId: { description: 'The test identifier.' },
+  },
+} satisfies Meta<typeof Table>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// the table data type
+type TableData = {
+  key: string;
+  value: string;
+};
+
+// the table column definitions
+const columnHelper = createColumnHelper<TableData>();
+const simpleTableColumns = [columnHelper.accessor('key', {}), columnHelper.accessor('value', {})];
+const decoratedTableColumns = [
+  columnHelper.accessor('key', {
+    cell: (info) => <span className="font-mono text-blue-600">{info.getValue()}</span>,
+    header: () => 'Key',
+  }),
+  columnHelper.accessor('value', {
+    header: () => 'Value',
+  }),
+];
+
+// the data to populate the table
+const myTableData: TableData[] = [
+  {
+    key: 'className',
+    value: 'Optional. Additional CSS class names.',
+  },
+  {
+    key: 'testId',
+    value: 'Optional. Identifier for testing.',
+  },
+];
+
+export const Simple: Story = {
+  args: {
+    // @ts-expect-error Storybook generics
+    columns: simpleTableColumns,
+    data: myTableData,
+  },
+  render: ({ columns, data }) => {
+    return (
+      <Table<TableData, string>
+        columns={columns as ColumnDef<TableData, string>[]}
+        data={data as TableData[]}
+      />
+    );
+  },
+};
+
+export const DecoratedColumns: Story = {
+  args: {
+    // @ts-expect-error Storybook generics
+    columns: decoratedTableColumns,
+    data: myTableData,
+  },
+  render: ({ columns, data }) => {
+    return (
+      <Table<TableData, string>
+        columns={columns as ColumnDef<TableData, string>[]}
+        data={data as TableData[]}
+      />
+    );
+  },
+};
