4dc20b81742d05b52cb19ca0fd8fac1431fd43d2
Matthew Warman <warmanmr@gmail.com>
2025-01-16 08:45:06 -0500

    donutchart stories

--- src/__fixtures__/charts.ts
+++ src/__fixtures__/charts.ts
@@ -1,12 +1,16 @@
+import colors from 'tailwindcss/colors';
+
 import { DonutChartData } from 'common/components/Chart/DonutChart';
 
 export const donutChartDataFixture: DonutChartData[] = [
   {
     name: 'Apples',
     value: 3,
+    fill: colors.red[700],
   },
   {
     name: 'Oranges',
     value: 7,
+    fill: colors.orange[300],
   },
 ];
