4d276fef9d27a927ec6b82b08b6ef59baba17529
Matthew Warman <warmanmr@gmail.com>
2025-02-10 11:35:04 -0500

    #28 tests

--- src/common/components/Form/__tests__/Input.test.tsx
+++ src/common/components/Form/__tests__/Input.test.tsx
@@ -1,18 +1,41 @@
 import { describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { InferType, object, string } from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { render, screen } from 'test/test-utils';
-import WithFormProvider from 'test/wrappers/WithFormProvider';
 
-import Input from '../Input';
+import Input, { InputProps } from '../Input';
+
+const formSchema = object({
+  color: string().required('Required'),
+});
+
+type FormValues = InferType<typeof formSchema>;
+
+const InputWrapper = (props: Omit<InputProps<FormValues>, 'control'>) => {
+  const form = useForm<FormValues>({
+    defaultValues: { color: '' },
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} data-testid="form">
+      <Input {...props} control={form.control} />
+      <button type="submit" data-testid="button-submit">
+        submit
+      </button>
+    </form>
+  );
+};
 
 describe('Input', () => {
   it('should render successfully', async () => {
     // ARRANGE
-    render(
-      <WithFormProvider formProps={{ defaultValues: { testField: '' } }}>
-        <Input name="testField" />
-      </WithFormProvider>,
-    );
+    render(<InputWrapper name="color" />);
     await screen.findByTestId('input');
 
     // ASSERT
@@ -22,11 +45,7 @@ describe('Input', () => {
   it('should show label', async () => {
     // ARRANGE
     const label = 'Label text';
-    render(
-      <WithFormProvider formProps={{ defaultValues: { testField: '' } }}>
-        <Input name="testField" label={label} />
-      </WithFormProvider>,
-    );
+    render(<InputWrapper name="color" label={label} />);
     await screen.findByTestId('input-label');
 
     // ASSERT
@@ -36,11 +55,7 @@ describe('Input', () => {
   it('should show supporting text', async () => {
     // ARRANGE
     const supportingText = 'Supporting text';
-    render(
-      <WithFormProvider formProps={{ defaultValues: { testField: '' } }}>
-        <Input name="testField" supportingText={supportingText} />
-      </WithFormProvider>,
-    );
+    render(<InputWrapper name="color" supportingText={supportingText} />);
     await screen.findByTestId('input-supporting-text');
 
     // ASSERT
@@ -49,20 +64,15 @@ describe('Input', () => {
 
   it('should show error message', async () => {
     // ARRANGE
-    const errorMessage = 'error message';
-    render(
-      <WithFormProvider
-        formProps={{
-          defaultValues: { testField: '' },
-          errors: { testField: { type: 'string', message: errorMessage } },
-        }}
-      >
-        <Input name="testField" />
-      </WithFormProvider>,
-    );
+    const user = userEvent.setup();
+    render(<InputWrapper name="color" label="Color" />);
+    await screen.findByTestId('button-submit');
+
+    // ACT
+    await user.click(screen.getByTestId('button-submit'));
     await screen.findByTestId('input-error');
 
     // ASSERT
-    expect(screen.getByTestId('input-error')).toHaveTextContent(errorMessage);
+    expect(screen.getByTestId('input-error')).toHaveTextContent(/required/i);
   });
 });
