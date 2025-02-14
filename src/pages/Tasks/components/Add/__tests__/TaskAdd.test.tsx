84f0bdafb56edfae6045b107785a71a1ea703f93
Matthew Warman <warmanmr@gmail.com>
2025-02-14 10:33:11 -0500

    #11 tests

--- src/pages/Tasks/components/Add/__tests__/TaskAdd.test.tsx
+++ src/pages/Tasks/components/Add/__tests__/TaskAdd.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import { UseQueryResult } from '@tanstack/react-query';
 
-import { render, screen } from 'test/test-utils';
+import { render, screen, waitFor } from 'test/test-utils';
 import * as UseGetCurrentUser from 'common/api/useGetCurrentUser';
 import { User } from 'common/api/useGetUser';
 import { userFixture1 } from '__fixtures__/users';
@@ -66,5 +66,27 @@ describe('TaskAdd', () => {
 
     // ASSERT
     expect(mockNavigate).toHaveBeenCalledWith(-1);
+  });
+
+  it('should display task create error', async () => {
+    // ARRANGE
+    const user = userEvent.setup();
+    const useGetCurrentUserSpy = vi.spyOn(UseGetCurrentUser, 'useGetCurrentUser');
+    useGetCurrentUserSpy.mockReturnValue({
+      data: userFixture1,
+    } as unknown as UseQueryResult<User>);
+    render(<TaskAdd />);
+    await screen.findByTestId('task-form-button-submit');
+
+    // ACT
+    await user.clear(screen.getByTestId('task-form-input-title-input'));
+    await user.type(screen.getByTestId('task-form-input-title-input'), '500');
+    await user.click(screen.getByTestId('task-form-button-submit'));
+    await waitFor(() => expect(screen.getByTestId('task-add-error-create')).toBeDefined());
+
+    // ASSERT
+    expect(screen.getByTestId('task-form-input-title-input')).toHaveValue('500');
+    expect(mockNavigate).not.toHaveBeenCalled();
+    expect(screen.getByTestId('task-add-error-create')).toBeDefined();
   });
 });
