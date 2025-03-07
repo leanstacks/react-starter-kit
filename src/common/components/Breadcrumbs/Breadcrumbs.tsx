f02ab02ccea77d9323f96dacf725c14290b4373e
Matthew Warman <warmanmr@gmail.com>
2025-03-07 08:38:29 -0500

    #39 docs

--- src/common/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/common/components/Breadcrumbs/Breadcrumbs.tsx
@@ -6,6 +6,11 @@ import { default as CommonLink, LinkProps as CommonLinkProps } from 'common/comp
 import FAIcon, { FAIconProps } from '../Icon/FAIcon';
 
 /**
+ * Properties for the `Breadcrumbs` component.
+ */
+export interface BreadcrumbsProps extends BaseComponentProps, PropsWithChildren {}
+
+/**
  * The `Breadcrumbs` component renders a heirarchy of links as a path to the
  * current route.
  */
@@ -13,7 +18,7 @@ const Breadcrumbs = ({
   children,
   className,
   testId = 'breadcrumbs',
-}: BaseComponentProps & PropsWithChildren): JSX.Element => {
+}: BreadcrumbsProps): JSX.Element => {
   return (
     <nav className={cn(className)} aria-label="breadcrumbs" data-testid={testId}>
       {children}
@@ -21,6 +26,10 @@ const Breadcrumbs = ({
   );
 };
 
+/**
+ * The `List` contains the list of items in the breadcrumbs. It is located immediately
+ * within the `Breadcrumbs` component and contains one to many `Item` components.
+ */
 const List = ({
   children,
   className,
@@ -37,19 +46,31 @@ const List = ({
 };
 Breadcrumbs.List = List;
 
+/**
+ * The `Item` represents a single element of the breadcrumbs list. An item typically
+ * represents a single element of the path to which a user may navigate.
+ *
+ * An `Item` may contain any type of child; however, an item typically contains a `Link`,
+ * `Page`, or a `DropdownMenu`.
+ */
 const Item = ({
   children,
   className,
   testId = 'breadcrumbs-item',
 }: BaseComponentProps & PropsWithChildren): JSX.Element => {
   return (
-    <li className={cn(className)} data-testid={testId}>
+    <li className={cn('last:font-bold', className)} data-testid={testId}>
       {children}
     </li>
   );
 };
 Breadcrumbs.Item = Item;
 
+/**
+ * A `Link` represents a navigation link item within breadcrumbs.  A link is
+ * a child of an `Item`.  Use the `to` property to specify the relative path for
+ * the link.
+ */
 const Link = ({
   children,
   className,
@@ -58,10 +79,7 @@ const Link = ({
 }: CommonLinkProps): JSX.Element => {
   return (
     <CommonLink
-      className={cn(
-        'text-light-text dark:text-dark-text block max-w-40 truncate opacity-90',
-        className,
-      )}
+      className={cn('text-light-text dark:text-dark-text block max-w-40 truncate', className)}
       data-testid={testId}
       {...props}
     >
@@ -71,19 +89,29 @@ const Link = ({
 };
 Breadcrumbs.Link = Link;
 
+/**
+ * A `Page` represents an item within breadcrumbs with no associated navigation.
+ * Use the `Page` as an alternative to the `Link` when the breadcrumb item is
+ * not clickable. A `Page` is commonly used for the current page, i.e. the last
+ * element in the breadcrumbs.
+ */
 const Page = ({
   children,
   className,
   testId = 'breadcrumbs-page',
 }: BaseComponentProps & PropsWithChildren): JSX.Element => {
   return (
-    <span className={cn('block max-w-40 truncate font-bold', className)} data-testid={testId}>
+    <span className={cn('block max-w-40 truncate', className)} data-testid={testId}>
       {children}
     </span>
   );
 };
 Breadcrumbs.Page = Page;
 
+/**
+ * The `Separator` component renders an icon which visually separates two
+ * breadcrumbs elements.  The default icon is a right chevron.
+ */
 const Separator = ({
   className,
   icon = 'chevronRight',
@@ -101,6 +129,14 @@ const Separator = ({
 };
 Breadcrumbs.Separator = Separator;
 
+/**
+ * The `Ellipsis` component renders a horizontal ellipsis icon. This is useful when
+ * the number of breadcrumbs elements is too great to render all of them. Use the
+ * ellipsis to represent breadcrumbs items which have been omitted to shorten the
+ * overall breadcrumbs list. Alternatively, use the ellipsis as the trigger for
+ * a `DropdownMenu` within the breadcrumbs to allow navigation to those routes
+ * which have been omitted for brevity.
+ */
 const Ellipsis = ({
   className,
   testId = 'breadcrumbs-ellipsis',
