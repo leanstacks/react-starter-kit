88d050ecbe1d7ae9244bf76024ac111b09b5c683
Matthew Warman <warmanmr@gmail.com>
2025-03-21 16:12:38 -0400

    #36 examples

--- src/pages/Components/components/AccordionComponents.tsx
+++ src/pages/Components/components/AccordionComponents.tsx
@@ -100,6 +100,97 @@ const AccordionComponents = ({
 </Accordion>`}
           />
         </div>
+
+        <Heading level={4} className="mb-2">
+          Content
+        </Heading>
+        <div className="mb-4 opacity-85">
+          The Accordion content may contain anything, such as text, images, forms, etc. There is no
+          limitation on the what may be placed inside the contennt of an Accordion.
+        </div>
+        <div className="my-8">
+          <div className="mb-2 flex place-content-center rounded-sm border border-neutral-500/10 p-4 dark:bg-neutral-700/25">
+            {/* Example */}
+            <Accordion className="w-full">
+              <Accordion.Item value="prologue">
+                <Accordion.Trigger>Prologue</Accordion.Trigger>
+                <Accordion.Content>
+                  <div className="mb-4 text-sm font-semibold">
+                    <span className="text-2xl">O</span>rnare occaecat aliquip voluptate esse est
+                    occaecat cillum nam. Eiusmod aliquip magna dapibus ut ut deserunt. Justo nisi mi
+                    nunc nunc cras. Ipsum quam officia dapibus et aenean enim. Bibendum do convallis
+                    donec eiusmod elit arcu est. Quam ero ea massa culpa adipiscing. Justo neque
+                    aute qui nisi. Convallis nisi et irure mollit dui aliqua fugait laoreet.
+                  </div>
+                  <div className="mb-4 text-sm">
+                    Mollit urna ut ut pulvinar ullamco. Ero enim eiusmod qui in velit. Posuere
+                    dapibus cras culpa ligula magna. Do neque urna nostrud tempus officia. Consequat
+                    condimentum a eiusmod cillum. Pulvinar elit qui pulvinar donec non aute dolor.
+                    Sunt aliquip dapibus mi vitae labore.
+                  </div>
+                  <div className="text-sm">
+                    Irure laoreet ligula justo sapien magna ea vel. Proident mollit aenean aliquip
+                    magna ullamco ipsum tempus incididunt. Do aenean eiusmod amet dolor ipsum sunt
+                    sit. Irure reprehenderit ad cupidatat labore bibendum convallis. Tempor deserunt
+                    nam lorem cras excepteur a bibendum egestas. Donec labore duis esse quam. Dui
+                    culpa cupidatat cras cillum vel ornare pulvinar dui. Duis donec aenean vel
+                    adipiscing ligula.
+                  </div>
+                </Accordion.Content>
+              </Accordion.Item>
+              <Accordion.Item value="chapter-1">
+                <Accordion.Trigger>Chapter 1</Accordion.Trigger>
+                <Accordion.Content>
+                  <div className="mb-4 text-sm font-semibold">
+                    <span className="text-2xl">E</span>gestas justo donec laboris justo id laborum
+                    aliqua. Nulla dapibus aute aenean donec vitae enim pulvinar donec. Neque
+                    cupidatat facilisis sint laboris magna ea. Anim ero proident laboris fugait.
+                    Tempus labore dui vel laborum esse donec pariatur. Consequat id laoreet mollit
+                    fugait id cillum anim.
+                  </div>
+                  <img src="https://placehold.co/400x600" alt="Placeholder" className="mb-4" />
+                  <div className="text-sm">
+                    Pulvinar e nibh veniam arcu at aliqua pariatur. Enim cillum cupidatat elit elit
+                    occaecat reprehenderit. Nibh sed justo quam convallis fermentum e aute.
+                    Incididunt ero in ornare anim voluptate incididunt nul. Do exercitation bibendum
+                    nulla lorem est nul. Irure labore ligula dui qos culpa aliquip condimentum.
+                    Proin excepteur incididunt do commodo deserunt egestas.
+                  </div>
+                </Accordion.Content>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <CodeSnippet
+            className="my-2"
+            code={`<Accordion className="w-full">
+  <Accordion.Item value="prologue">
+    <Accordion.Trigger>Prologue</Accordion.Trigger>
+    <Accordion.Content>
+      <div className="mb-4 text-sm font-semibold">
+        <span className="text-2xl">O</span>rnare occaecat aliquip voluptate esse...
+      </div>
+      <div className="mb-4 text-sm">
+        Mollit urna ut ut pulvinar ullamco. Ero enim eiusmod qui in velit. Posuere...
+      <div className="mb-4 text-sm">
+        Irure laoreet ligula justo sapien magna ea vel. Proident mollit aenean...
+      </div>
+    </Accordion.Content>
+  </Accordion.Item>
+  <Accordion.Item value="chapter-1">
+    <Accordion.Trigger>Chapter 1</Accordion.Trigger>
+    <Accordion.Content>
+      <div className="mb-4 text-sm font-semibold">
+        <span className="text-2xl">E</span>gestas justo donec laboris justo id...
+      </div>
+      <img src="https://placehold.co/400x200" alt="Placeholder" className="mb-4" />
+      <div className="mb-4 text-sm">
+        Pulvinar e nibh veniam arcu at aliqua pariatur. Enim cillum cupidatat...
+      </div>
+    </Accordion.Content>
+  </Accordion.Item>
+</Accordion>`}
+          />
+        </div>
       </div>
     </section>
   );
