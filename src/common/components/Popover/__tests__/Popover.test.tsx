d689c4feb780ca09eb728e53b280a7230901bf33
Matthew Warman <warmanmr@gmail.com>
2025-03-28 08:38:53 -0400

    #68 tests

--- src/common/components/Popover/__tests__/Popover.test.tsx
+++ src/common/components/Popover/__tests__/Popover.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
 import { render, screen, waitFor } from 'test/test-utils';
@@ -88,6 +88,21 @@ describe('Popover', () => {
 
   it('should handle default Popover placement', async () => {
     // ARRANGE
+    const user = userEvent.setup();
+    vi.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(
+      () =>
+        ({
+          height: 10,
+          width: 20,
+          top: 100,
+          left: 100,
+          right: 120,
+          bottom: 110,
+          x: 100,
+          y: 100,
+          toJSON: () => ({}),
+        }) as DOMRect,
+    );
     render(
       <Popover>
         <Popover.Trigger>Open</Popover.Trigger>
@@ -96,12 +111,37 @@ describe('Popover', () => {
     );
     await screen.findByTestId('popover-content');
 
+    // ACT
+    await user.click(screen.getByTestId('popover-trigger'));
+    await waitFor(() =>
+      expect(screen.getByTestId('popover-content')).toHaveStyle({
+        transform: 'translate(100px, 118px)',
+      }),
+    );
+
     // ASSERT
-    expect(screen.getByTestId('popover-content')).toHaveStyle({ transform: 'translate(0px, 8px)' });
+    expect(screen.getByTestId('popover-content')).toHaveStyle({
+      transform: 'translate(100px, 118px)',
+    });
   });
 
   it('should handle bottom Popover placement', async () => {
     // ARRANGE
+    const user = userEvent.setup();
+    vi.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(
+      () =>
+        ({
+          height: 10,
+          width: 20,
+          top: 100,
+          left: 100,
+          right: 120,
+          bottom: 110,
+          x: 100,
+          y: 100,
+          toJSON: () => ({}),
+        }) as DOMRect,
+    );
     render(
       <Popover>
         <Popover.Trigger>Open</Popover.Trigger>
@@ -110,12 +150,37 @@ describe('Popover', () => {
     );
     await screen.findByTestId('popover-content');
 
+    // ACT
+    await user.click(screen.getByTestId('popover-trigger'));
+    await waitFor(() =>
+      expect(screen.getByTestId('popover-content')).toHaveStyle({
+        transform: 'translate(100px, 118px)',
+      }),
+    );
+
     // ASSERT
-    expect(screen.getByTestId('popover-content')).toHaveStyle({ transform: 'translate(0px, 8px)' });
+    expect(screen.getByTestId('popover-content')).toHaveStyle({
+      transform: 'translate(100px, 118px)',
+    });
   });
 
   it('should handle top Popover placement', async () => {
     // ARRANGE
+    const user = userEvent.setup();
+    vi.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(
+      () =>
+        ({
+          height: 10,
+          width: 20,
+          top: 100,
+          left: 100,
+          right: 120,
+          bottom: 110,
+          x: 100,
+          y: 100,
+          toJSON: () => ({}),
+        }) as DOMRect,
+    );
     render(
       <Popover>
         <Popover.Trigger>Open</Popover.Trigger>
@@ -124,14 +189,37 @@ describe('Popover', () => {
     );
     await screen.findByTestId('popover-content');
 
+    // ACT
+    await user.click(screen.getByTestId('popover-trigger'));
+    await waitFor(() =>
+      expect(screen.getByTestId('popover-content')).toHaveStyle({
+        transform: 'translate(100px, 82px)',
+      }),
+    );
+
     // ASSERT
     expect(screen.getByTestId('popover-content')).toHaveStyle({
-      transform: 'translate(0px, -8px)',
+      transform: 'translate(100px, 82px)',
     });
   });
 
   it('should handle left Popover placement', async () => {
     // ARRANGE
+    const user = userEvent.setup();
+    vi.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(
+      () =>
+        ({
+          height: 10,
+          width: 20,
+          top: 100,
+          left: 100,
+          right: 120,
+          bottom: 110,
+          x: 100,
+          y: 100,
+          toJSON: () => ({}),
+        }) as DOMRect,
+    );
     render(
       <Popover>
         <Popover.Trigger>Open</Popover.Trigger>
@@ -140,14 +228,37 @@ describe('Popover', () => {
     );
     await screen.findByTestId('popover-content');
 
+    // ACT
+    await user.click(screen.getByTestId('popover-trigger'));
+    await waitFor(() =>
+      expect(screen.getByTestId('popover-content')).toHaveStyle({
+        transform: 'translate(72px, 100px)',
+      }),
+    );
+
     // ASSERT
     expect(screen.getByTestId('popover-content')).toHaveStyle({
-      transform: 'translate(-8px, 0px)',
+      transform: 'translate(72px, 100px)',
     });
   });
 
   it('should handle right Popover placement', async () => {
     // ARRANGE
+    const user = userEvent.setup();
+    vi.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(
+      () =>
+        ({
+          height: 10,
+          width: 20,
+          top: 100,
+          left: 100,
+          right: 120,
+          bottom: 110,
+          x: 100,
+          y: 100,
+          toJSON: () => ({}),
+        }) as DOMRect,
+    );
     render(
       <Popover>
         <Popover.Trigger>Open</Popover.Trigger>
@@ -156,9 +267,17 @@ describe('Popover', () => {
     );
     await screen.findByTestId('popover-content');
 
+    // ACT
+    await user.click(screen.getByTestId('popover-trigger'));
+    await waitFor(() =>
+      expect(screen.getByTestId('popover-content')).toHaveStyle({
+        transform: 'translate(128px, 100px)',
+      }),
+    );
+
     // ASSERT
     expect(screen.getByTestId('popover-content')).toHaveStyle({
-      transform: 'translate(8px, 0px)',
+      transform: 'translate(128px, 100px)',
     });
   });
 
