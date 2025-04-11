5938c3d816bcebeb2a33a6d6518b8ed8f05c64a7
Matthew Warman <warmanmr@gmail.com>
2025-04-11 06:23:02 -0400

    #94 Label cva

--- src/common/components/Form/Label.tsx
+++ src/common/components/Form/Label.tsx
@@ -1,7 +1,23 @@
 import { LabelHTMLAttributes } from 'react';
+import { cva } from 'class-variance-authority';
 
 import { cn } from 'common/utils/css';
 import { BaseComponentProps } from 'common/utils/types';
+
+/**
+ * Define the `Label` component base and variant styles.
+ */
+const labelVariants = cva('mb-1 block text-sm font-medium', {
+  variants: {
+    required: {
+      true: 'font-bold after:content-["*"]',
+      false: '',
+    },
+  },
+  defaultVariants: {
+    required: false,
+  },
+});
 
 /**
  * Properties for the `Label` component.
@@ -17,8 +33,6 @@ export interface LabelProps extends BaseComponentProps, LabelHTMLAttributes<HTML
 /**
  * The `Label` component renders a HTML `label` element. It is used to describe
  * a form control.
- * @param {LabelProps} props - Component properties.
- * @returns JSX
  */
 const Label = ({
   className,
@@ -27,15 +41,7 @@ const Label = ({
   ...props
 }: LabelProps): JSX.Element => {
   return (
-    <label
-      className={cn(
-        'mb-1 block text-sm font-medium',
-        { 'font-bold after:content-["*"]': required },
-        className,
-      )}
-      data-testid={testId}
-      {...props}
-    />
+    <label className={cn(labelVariants({ required }), className)} data-testid={testId} {...props} />
   );
 };
 
